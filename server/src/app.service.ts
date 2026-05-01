84f845c06d3687301fd8d7f35ea61d69a02f4038
Flyan <guilhermeflyan.22@gmail.com>
2026-05-01 20:55:38 -0300

    Update app.service.ts

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -2,7 +2,22 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  
+  name: string;
+  version: string;
+  status: boolean;
+
+  constructor(){
+    this.name = "Gatriz Doces API";
+    this.version = "1.0.0";
+    this.status = true;
   }
+  
+  getHello(): {name: string, version: string, status: boolean} {
+    const name: string = this.name;
+    const version: string = this.version;
+    const status: boolean = this.status;
+    return {name, version, status};
+  }
+
 }
